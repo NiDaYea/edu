fe197e80c1dca6d0d2a6fd2a8e61ebd97bf5beb6
NiDaYea <878786052@qq.com>
2019-09-11 12:17:35 +0800

    刷题

--- edu-provider/src/main/java/cn/tcmp/first/service/TopicServiceImpl.java
+++ edu-provider/src/main/java/cn/tcmp/first/service/TopicServiceImpl.java
@@ -32,6 +32,6 @@ public class TopicServiceImpl implements TopicService {
 
     @Override
     public List<Topic> queryByYIbai(Integer topicCode) {
-        return null;
+        return topicMapper.queryByYIbai(topicCode);
     }
 }
