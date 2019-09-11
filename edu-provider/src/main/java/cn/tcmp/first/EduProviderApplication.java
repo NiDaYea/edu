a9dc0da709b1cfa36f8342df59a10d84e69583d3
zhang <1484591634@qq.com>
2019-09-11 12:19:24 +0800

    请大家及时更新

--- edu-provider/src/main/java/cn/tcmp/first/EduProviderApplication.java
+++ edu-provider/src/main/java/cn/tcmp/first/EduProviderApplication.java
@@ -1,5 +1,6 @@
 package cn.tcmp.first;
 
+import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
--- edu-provider/src/main/java/cn/tcmp/first/dao/UserMapper.java
+++ edu-provider/src/main/java/cn/tcmp/first/dao/UserMapper.java
@@ -1,4 +1,9 @@
 package cn.tcmp.first.dao;
 
+import cn.tcmp.first.entity.User;
+
 public interface UserMapper {
+
+    //在线报名
+    Integer saveUser(User user);
 }
